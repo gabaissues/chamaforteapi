dbc97bf35d91f5981a7a6f6b8b42b3e99d0e07e1
gaba <gaba@pop-os.localdomain>
2022-12-21 11:23:00 -0300

    Payment logs add

--- src/controllers/Payment.controllers.ts
+++ src/controllers/Payment.controllers.ts
@@ -5,6 +5,7 @@ import OrdersModal from '../models/Orders.models'
 import PaymentModal from '../models/Payment.models'
 import UsersModal from '../models/Users.models'
 import { IStock } from '../interfaces/Stock.interfaces'
+import LogsModal from '../models/Logs.models'
 
 const modal = new PaymentModal()
 const ordersModal = new OrdersModal()
@@ -26,17 +27,17 @@ export default class PaymentController {
 
     public async create(req: Request, res: Response) {
 
-        if(!req.query.id || !req.query.topic) return res.status(206).send({ message: 'Falta contéudo na QUERY...' })
-        if(req.query.topic != "payment") return res.status(404).send({ message: 'O tópico não é de pagamento...' })
+        if (!req.query.id || !req.query.topic) return res.status(206).send({ message: 'Falta contéudo na QUERY...' })
+        if (req.query.topic != "payment") return res.status(404).send({ message: 'O tópico não é de pagamento...' })
 
         try {
 
             const paymentMp = await modal.getPaymentByIdUsingMP(Number(req.query.id))
             const paymentDb = await modal.getPaymentByPaymentId(`${paymentMp.response.collector_id}`)
 
-            if(paymentMp.body.status != "approved") return res.status(404).send({ message: 'O pagamento não foi aprovado...' })
-            
-            res.status(200).send({ message: 'Pagamento CRIADO com sucesso.'})
+            if (paymentMp.body.status != "approved") return res.status(404).send({ message: 'O pagamento não foi aprovado...' })
+
+            res.status(200).send({ message: 'Pagamento CRIADO com sucesso.' })
 
             //Buscando os ORDERS
             const orders = await ordersModal.getOrdersByEmail(paymentDb.tech)
@@ -46,7 +47,7 @@ export default class PaymentController {
             //O = Order
             orders.forEach((o) => {
 
-                ordersModal.editOrderById(o._id, { 
+                ordersModal.editOrderById(o._id, {
                     id: o.id,
                     client: o.client,
                     images: o.images,
@@ -60,12 +61,12 @@ export default class PaymentController {
                     pieces: o.pieces,
                     status: 'pago'
                 })
-                
+
             })
 
             //Buscando o técnico 
             const tech = await usersModal.getUserByEmail(paymentDb.tech)
-            if(!tech) return res.status(404).send({ message: 'Não foi encontrado o técnico' })
+            if (!tech) return res.status(404).send({ message: 'Não foi encontrado o técnico' })
 
             //Novo estoque
             const stock: IStock[] = []
@@ -74,13 +75,13 @@ export default class PaymentController {
             tech.stock.forEach((s) => {
                 paymentDb.pieces.split(',').forEach((p) => {
 
-                    if(s.piece === p) {
-                        
-                        stock.push({ piece: s.piece, quantity: s.quantity - 1, value: s.value, pieceId: s.pieceId  })
+                    if (s.piece === p) {
+
+                        stock.push({ piece: s.piece, quantity: s.quantity - 1, value: s.value, pieceId: s.pieceId })
 
                     } else {
 
-                        stock.push({ piece: s.piece, quantity: s.quantity, value: s.value, pieceId: s.pieceId  })
+                        stock.push({ piece: s.piece, quantity: s.quantity, value: s.value, pieceId: s.pieceId })
 
                     }
 
@@ -88,7 +89,7 @@ export default class PaymentController {
             })
 
             //Editando o técnico
-            await usersModal.editUserByEmail(paymentDb.tech, { 
+            await usersModal.editUserByEmail(paymentDb.tech, {
                 name: tech.name,
                 telephone: tech.telephone,
                 documentation: tech.documentation,
@@ -103,9 +104,14 @@ export default class PaymentController {
                 invoices: tech.invoices,
                 location: tech.location,
                 statistics: tech.statistics,
-                stock: stock  
+                stock: stock
             })
 
+            new LogsModal().create({
+                type: 'info',
+                message: `O ${tech.name} teve suas faturas pagas no dia ${new Date().toLocaleString('pt-br')}, e as peças usadas já foram removidas do banco de dados.`,
+                date: new Date().toLocaleString('pt-br')
+            })
 
             /*
             
@@ -118,7 +124,7 @@ export default class PaymentController {
 
             */
 
-        } catch(e) {
+        } catch (e) {
 
             res.status(501).send({ message: e })
 
@@ -127,7 +133,7 @@ export default class PaymentController {
 
     public async delete(req: Request, res: Response) {
 
-        
+
 
     }
 }
