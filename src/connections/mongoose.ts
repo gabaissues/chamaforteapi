3a21ae33b4feb183d03857d425af02c6bb2fe3a9
gaba <gaba@pop-os.localdomain>
2022-12-28 19:23:36 -0300

    Adding mongoose

--- src/connections/mongoose.ts
+++ src/connections/mongoose.ts
@@ -1,2 +1,2 @@
 import mongoose from 'mongoose'
-mongoose.connect('mongodb+srv://gbxxy:ZJvLOv9B4K1c6AS1@database.cnddg.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(`${process.env.MongoUrl}`)
